878eb8e534266f693c0517856e0b4c8734e25806
aszanto9 <aszanto@college.harvard.edu>
2017-03-02 19:25:45 -0500

    getting started up

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lsm-tree
+//
+//  Created by Aron Szanto on 3/2/17.
+//  Copyright © 2017 Aron Szanto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

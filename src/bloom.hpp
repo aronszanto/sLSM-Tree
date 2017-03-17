9ca90b10993c40300d494d527dc78c4925e9a5ed
aszanto9 <aszanto@college.harvard.edu>
2017-03-16 21:51:03 -0500

    woot bloom works

--- src/bloom.hpp
+++ src/bloom.hpp
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <cstdint>
 #include <vector>
+#include <array>
 #include "MurmurHash.h"
 
 using namespace std;
@@ -19,12 +20,40 @@ using namespace std;
 template<class Key, class Hash = hash<Key>>
 class BloomFilter {
 public:
-    BloomFilter(uint64_t size, uint8_t numHashes)
-    : m_bits(size),
-    m_numHashes(numHashes) {}
+    BloomFilter(uint64_t size, uint8_t numHashes): m_bits(size), m_numHashes(numHashes) {}
     
-    void add(const uint8_t *data, size_t len);
-    bool possiblyContains(const Key *data, size_t len) const;
+    array<uint64_t, 2> hash(const Key *data, size_t len) {
+        
+        array<uint64_t, 2> hashValue;
+        
+        MurmurHash3_x64_128(data, len, 0, hashValue.data());
+        
+        return hashValue;
+    }
+    
+    uint64_t nthHash(uint8_t n, uint64_t hashA, uint64_t hashB, uint64_t filterSize) {
+        return (hashA + n * hashB) % filterSize;
+    }
+    
+    void add(const Key *data, size_t len) {
+        auto hashValues = hash(data, len);
+        
+        for (int n = 0; n < m_numHashes; n++) {
+            m_bits[nthHash(n, hashValues[0], hashValues[1], m_bits.size())] = true;
+        }
+    }
+    
+    bool mayContain(const Key *data, size_t len) {
+        auto hashValues = hash(data, len);
+        
+        for (int n = 0; n < m_numHashes; n++) {
+            if (!m_bits[nthHash(n, hashValues[0], hashValues[1], m_bits.size())]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
     
 private:
     uint8_t m_numHashes;
--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,67 @@
 #include <cstdio>
 #include <ctime>
 #include <iostream>
+#include <math.h>
+#include <random>
+
 #include "skipList.hpp"
 #include "bloom.hpp"
 #include "lsm.hpp"
 
+
 using namespace std;
+
+
+void bloomFilterTest(){
+    std::random_device                  rand_dev;
+    std::mt19937                        generator(rand_dev());
+    std::uniform_int_distribution<int>  distribution(INT32_MIN, INT32_MAX);
+  
+    const int num_inserts = 100000;
+    double fprate = .04;
+    double denom = 0.480453013918201; // (ln(2))^2
+    uint64_t size = -1 * num_inserts * (log(fprate) / denom);
+    
+    double ln2 = 0.693147180559945;
+    
+    uint8_t numHashes = (int) ceil( (size / num_inserts) * ln2);  // ln(2)
+    
+    printf("%llu,%i\n", size, numHashes);
+    BloomFilter<int32_t> bf = BloomFilter<int32_t>(size, numHashes);
+    
+    std::vector<int> to_insert;
+    for (int i = 0; i < num_inserts; i++) {
+        int insert = distribution(generator);
+        to_insert.push_back(insert);
+    }
+    std::clock_t    start_insert;
+    std::cout << "Starting inserts" << std::endl;
+    start_insert = std::clock();
+    for (int i = 0; i < num_inserts; i++) {
+        bf.add(&i, sizeof(i));
+    }
+    
+    double total_insert = (std::clock() - start_insert) / (double)(CLOCKS_PER_SEC);
+    
+    std::cout << "Time: " << total_insert << " s" << std::endl;
+    std::cout << "Inserts per second: " << (int) num_inserts / total_insert << " s" << std::endl;
+    int fp = 0;
+    for (int i = num_inserts; i < 2 * num_inserts; i++) {
+        bool lookup = bf.mayContain(&i, sizeof(i));
+        if (lookup){
+            cout << i << " found but didn't exist" << endl;
+            fp++;
+        }
+    }
+    cout << fp << endl;
+    cout << "FP rate: " << ((double) fp / double(num_inserts)) << endl;
+
+    
+    
+    
+    
+    
+}
 void insertLookupTest(){
     std::random_device                  rand_dev;
     std::mt19937                        generator(rand_dev());
@@ -58,7 +114,7 @@ void insertLookupTest(){
 int main(){
 
     
-    
+    bloomFilterTest();
     return 0;
     
 }
